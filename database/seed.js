95b87a5c83e6d2ff64bcffdc6df04fb2621d8782
Jason Horn <jasonh.horn1@gmail.com>
2021-01-14 15:44:06 -0800

    react dir

--- database/seed.js
+++ database/seed.js
@@ -1,19 +1,14 @@
-const db  = require('./index.js');
-const Listing = require('./Listing.js');
 const faker = require('faker');
+const db = require('./index.js');
+const Listing = require('./Listing.js');
 
 const sampleListings = [];
 
-const generateRandomNumber = (max) => {
-  return Math.floor(Math.random() * Math.floor(max));
-}
+const generateRandomNumber = (max) => Math.floor(Math.random() * Math.floor(max));
 
-const generatePriceChange = () => {
-  let options = ['noChanges', 'raised', 'lowered'];
-  return options[generateRandomNumber(3)];
-}
+const generatePriceChange = () => ['noChanges', 'raised', 'lowered'][generateRandomNumber(3)];
 
-for (let i = 0; i < 10; i ++) {
+for (let i = 0; i < 100; i += 1) {
   sampleListings.push({
     images: ['https://image.shutterstock.com/image-photo/old-dilapidated-shack-600w-1061005514.jpg', 'https://image.shutterstock.com/image-photo/one-room-shack-interior-pioneer-260nw-399081187.jpg', 'https://image.shutterstock.com/image-photo/isolates-toilet-roofs-concrete-timber-600w-233727097.jpg'],
     price: faker.commerce.price(),
@@ -21,15 +16,15 @@ for (let i = 0; i < 10; i ++) {
     bedrooms: (generateRandomNumber(6) + 1),
     bathrooms: (generateRandomNumber(6) + 1),
     addressLineOne: faker.address.streetAddress(),
-    addressLineTwo: faker.address.city() + ',' + faker.address.stateAbbr(),
+    addressLineTwo: `${faker.address.city()}, ${faker.address.stateAbbr()}`,
     agency: faker.company.companyName(),
     recentPriceChange: generatePriceChange(),
     new: faker.random.boolean(),
-    forSaleByOwner: faker.random.boolean()
-  })
+    forSaleByOwner: faker.random.boolean(),
+  });
 }
 
-const insertSampleListings = function() {
+const insertSampleListings = () => {
   Listing.create(sampleListings)
     .then(() => db.disconnect());
 };
