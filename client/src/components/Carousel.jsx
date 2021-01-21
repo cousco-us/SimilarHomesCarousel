60c04a11c01cfb9b88c133d3b33372eb4c4ad35e
Jason Horn <jasonh.horn1@gmail.com>
2021-01-21 13:50:57 -0800

    previous button

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
 import Listing from './Listing.jsx';
 import ListingsEnd from './ListingsEnd.jsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 // eslint-disable-next-line arrow-body-style
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: 0,
+      view: 1,
     };
+    this.nextButtonClick = this.nextButtonClick.bind(this);
+    this.previousButtonClick = this.previousButtonClick.bind(this);
+  }
+
+  nextButtonClick() {
+    const { view } = this.state;
+    this.setState({ view: (view + 1) });
+  }
+
+  previousButtonClick() {
+    const { view } = this.state;
+    this.setState({ view: (view - 1) });
+  }
+
+  nextButtonOrNot() {
+    const { view } = this.state;
+
+  }
+
+  previousButtonOrNot() {
+    const { view } = this.state;
+    if (view !== 0) {
+      return (
+        <div className="directionControl">
+          <button onClick={() => this.previousButtonClick()} className="carouselButtons">
+            <div className="buttonIcon">
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </div>
+          </button>
+        </div>
+      )
+    }
   }
 
   render() {
@@ -24,17 +58,15 @@ class Carousel extends React.Component {
       <div className="carousel">
         <div className="carouselContainer">
           <div className="carouselWindow">
-            <div style={gridStyle} className="carouselGridContainer">
-              <ul className="carouselList">
+              <ul className="carouselList" style={gridStyle}>
                 {
                 // eslint-disable-next-line max-len
                   fifteenListings.map((listing) => <Listing key={listing._id} like={like} unlike={unlike} listing={listing} />)
                 }
                 <ListingsEnd city={city} />
               </ul>
-            </div>
           </div>
-          {/* {conditionally render <div className="directionControl"><button className="carouselButtons"><div className="buttonIcon">fontAwesomeElement</div></button></div>} */}
+          {this.previousButtonOrNot()}
         </div>
       </div>
     );
